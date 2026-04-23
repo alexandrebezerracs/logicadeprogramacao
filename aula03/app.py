29744950ee91c5047d71b11adcca96f8a10b9725
andrepiress61 <alexandrebezerracs@outlook.com>
2026-04-23 12:15:12 -0300

    first commit

---
+++ aula03/app.py
@@ -0,0 +1,23 @@
+#NOTE: Boletim Escolar
+
+import os
+
+print(30*"-""Boletim Escolar", 30*"-")
+
+listadenotas =[]
+nome =input("Digite o nome do Aluno: ").title()
+curso=input("Digite o curso: ").upper()
+
+while True:
+    nota=float(input("Digite um nota: "))
+    listadenotas.append(nota)
+    print(listadenotas)
+
+
+    opcao=input("Deseja adicionar mais notas? (enter - sim | n - não)").lower()
+
+    if opcao =="n":
+        break
+media=sum(listadenotas)/len(listadenotas)
+
+print(media)
---
+++ aula03/app2.py
@@ -0,0 +1,13 @@
+#estrutura de dados em python
+
+#Lista | Dicionário | Tupla
+[
+[0,1,2],
+[0,1,2],
+[0,1,2],
+]
+[
+['','',''],
+['','',''],
+['','',''],
+]
